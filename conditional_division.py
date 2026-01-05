c4b0cc65892d0f8411a6c7f8d7835bead65712af
Sam12354 <161862599+Sam12354@users.noreply.github.com>
2026-01-05 12:37:57 +0200

    Update conditional_division.py

--- conditional_division.py
+++ conditional_division.py
@@ -1,7 +1,7 @@
 def division(num):
 
     if num % 10 == 0:
-        # in opython there's is not "==="
+        # in python there's is not "==="
         print("The number is divisible by 10")
     elif num % 7 == 0:
         print("The number is divisible by 7")
@@ -17,4 +17,5 @@ def division(num):
 # division(30)
 # division(15)
 # division(12)
+
 # division(1643)
